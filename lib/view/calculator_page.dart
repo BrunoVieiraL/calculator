2f9a0669dce6ba4b2a0ae64a621736fac08a1905
BrunoVieiraL <bruno.leo516@hotmail.com>
2022-06-14 16:22:58 -0300

    last_commit

--- lib/view/calculator_page.dart
+++ lib/view/calculator_page.dart
@@ -12,7 +12,6 @@ class CalculatorPage extends StatefulWidget {
 class _CalculatorPageState extends State<CalculatorPage> {
   String mensagemVisor = '0';
 
-  //Método para receber os valores e os operadores e criar a expressão que será repassada para a lib
   void setarValor(String valor) {
     setState(() {
       if (mensagemVisor == '0') {
@@ -23,22 +22,16 @@ class _CalculatorPageState extends State<CalculatorPage> {
     });
   }
 
-  //Limpa o visor da calculadora
   void resetar() {
     setState(() {
       mensagemVisor = '0';
     });
   }
 
-  //Realizar cálculo com equação repassada
   void realizarCalculo() {
-    //Se houver divisão trocar o caractere ÷ pelo da barra que é reconhecido pela instrução
-    mensagemVisor = mensagemVisor.replaceAll('÷', '/');
     Parser parser = Parser();
-    //Nesta linha a expressão matemática será criada de acordo com o que for passado na string
     Expression expressao = parser.parse(mensagemVisor);
-    ContextModel contextModel = ContextModel(); //Obtem o contexto da expressão
-    //Nesta linha será realisado de fato o cálculo
+    ContextModel contextModel = ContextModel();
     double resolucaoDaExpressao =
         expressao.evaluate(EvaluationType.REAL, contextModel);
     setState(() {
@@ -71,123 +64,121 @@ class _CalculatorPageState extends State<CalculatorPage> {
                 ),
               ),
             ),
-            Container(
-              child: Expanded(
-                child: Column(
-                  children: [
-                    Expanded(
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          CustomButton(
-                              keyText: 'C',
-                              keyColor: Colors.blue,
-                              onPress: () => {resetar()}),
-                          CustomButton(
-                              keyText: 'DEL',
-                              keyColor: Colors.blue,
-                              onPress: () => {resetar()}),
-                          CustomButton(
-                              keyText: '%',
-                              keyColor: Colors.blue,
-                              onPress: () => {setarValor('%')}),
-                          CustomButton(
-                              keyText: '÷',
-                              keyColor: Colors.blue,
-                              onPress: () => {setarValor('÷')}),
-                        ],
-                      ),
+            Expanded(
+              child: Column(
+                children: [
+                  Expanded(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        CustomButton(
+                            keyText: 'C',
+                            keyColor: Colors.blue,
+                            onPress: () => {resetar()}),
+                        CustomButton(
+                            keyText: 'DEL',
+                            keyColor: Colors.blue,
+                            onPress: () => {resetar()}),
+                        CustomButton(
+                            keyText: '%',
+                            keyColor: Colors.blue,
+                            onPress: () => {setarValor('%')}),
+                        CustomButton(
+                            keyText: '/',
+                            keyColor: Colors.blue,
+                            onPress: () => {setarValor('÷')}),
+                      ],
                     ),
-                    Expanded(
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          CustomButton(
-                              keyColor: Colors.amber,
-                              keyText: '7',
-                              onPress: () => {setarValor('7')}),
-                          CustomButton(
-                              keyColor: Colors.amber,
-                              keyText: '8',
-                              onPress: () => {setarValor('8')}),
-                          CustomButton(
-                              keyColor: Colors.amber,
-                              keyText: '9',
-                              onPress: () => {setarValor('9')}),
-                          CustomButton(
-                              keyText: '*',
-                              keyColor: Colors.blue,
-                              onPress: () => {setarValor('*')}),
-                        ],
-                      ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        CustomButton(
+                            keyColor: Colors.amber,
+                            keyText: '7',
+                            onPress: () => {setarValor('7')}),
+                        CustomButton(
+                            keyColor: Colors.amber,
+                            keyText: '8',
+                            onPress: () => {setarValor('8')}),
+                        CustomButton(
+                            keyColor: Colors.amber,
+                            keyText: '9',
+                            onPress: () => {setarValor('9')}),
+                        CustomButton(
+                            keyText: '*',
+                            keyColor: Colors.blue,
+                            onPress: () => {setarValor('*')}),
+                      ],
                     ),
-                    Expanded(
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          CustomButton(
-                              keyColor: Colors.amber,
-                              keyText: '4',
-                              onPress: () => {setarValor('4')}),
-                          CustomButton(
-                              keyColor: Colors.amber,
-                              keyText: '5',
-                              onPress: () => {setarValor('5')}),
-                          CustomButton(
-                              keyColor: Colors.amber,
-                              keyText: '6',
-                              onPress: () => {setarValor('6')}),
-                          CustomButton(
-                              keyText: '+',
-                              keyColor: Colors.blue,
-                              onPress: () => {setarValor('+')}),
-                        ],
-                      ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        CustomButton(
+                            keyColor: Colors.amber,
+                            keyText: '4',
+                            onPress: () => {setarValor('4')}),
+                        CustomButton(
+                            keyColor: Colors.amber,
+                            keyText: '5',
+                            onPress: () => {setarValor('5')}),
+                        CustomButton(
+                            keyColor: Colors.amber,
+                            keyText: '6',
+                            onPress: () => {setarValor('6')}),
+                        CustomButton(
+                            keyText: '+',
+                            keyColor: Colors.blue,
+                            onPress: () => {setarValor('+')}),
+                      ],
                     ),
-                    Expanded(
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          CustomButton(
-                              keyColor: Colors.amber,
-                              keyText: '1',
-                              onPress: () => {setarValor('1')}),
-                          CustomButton(
-                              keyColor: Colors.amber,
-                              keyText: '2',
-                              onPress: () => {setarValor('2')}),
-                          CustomButton(
-                              keyColor: Colors.amber,
-                              keyText: '3',
-                              onPress: () => {setarValor('3')}),
-                          CustomButton(
-                              keyText: '-',
-                              keyColor: Colors.blue,
-                              onPress: () => {setarValor('-')}),
-                        ],
-                      ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        CustomButton(
+                            keyColor: Colors.amber,
+                            keyText: '1',
+                            onPress: () => {setarValor('1')}),
+                        CustomButton(
+                            keyColor: Colors.amber,
+                            keyText: '2',
+                            onPress: () => {setarValor('2')}),
+                        CustomButton(
+                            keyColor: Colors.amber,
+                            keyText: '3',
+                            onPress: () => {setarValor('3')}),
+                        CustomButton(
+                            keyText: '-',
+                            keyColor: Colors.blue,
+                            onPress: () => {setarValor('-')}),
+                      ],
                     ),
-                    Expanded(
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          CustomButton(
-                              keyText: '0',
-                              keyColor: Colors.amber,
-                              onPress: () => {setarValor('0')}),
-                          CustomButton(
-                              keyText: '.',
-                              keyColor: Colors.amber,
-                              onPress: () => {setarValor('.')}),
-                          CustomButton(
-                              keyText: '=',
-                              keyColor: Colors.blue,
-                              onPress: () => realizarCalculo()),
-                        ],
-                      ),
+                  ),
+                  Expanded(
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        CustomButton(
+                            keyText: '0',
+                            keyColor: Colors.amber,
+                            onPress: () => {setarValor('0')}),
+                        CustomButton(
+                            keyText: '.',
+                            keyColor: Colors.amber,
+                            onPress: () => {setarValor('.')}),
+                        CustomButton(
+                            keyText: '=',
+                            keyColor: Colors.blue,
+                            onPress: () => realizarCalculo()),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
           ],
